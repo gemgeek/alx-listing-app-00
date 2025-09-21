450b5f6e06a75fbb69c98d3e840aee97f85d5b39
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-21 14:00:36 +0100

    feat: Set up project structure and layout components

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,10 +1,22 @@
-export interface CardProps {
-  title: string;
-  description: string;
-  imageUrl: string;
+export interface AddressProps {
+  state: string;
+  city: string;
+  country: string;
 }
 
-export interface ButtonProps {
-  label: string;
-  onClick?: () => void;
+export interface OffersProps {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+
+export interface PropertyProps {
+  name: string;
+  address: AddressProps;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: OffersProps;
+  image: string;
+  discount: string;
 }
